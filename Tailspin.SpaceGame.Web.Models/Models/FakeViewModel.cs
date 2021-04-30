60a841e6a890ad4efa970085adc19d572c7493ae
bchornii <bogdan.chorniy@gmail.com>
2021-04-30 08:09:15 +0300

    Feature/foo11 (#10)
    * update model on foo11
    
    * update model on foo11 +semver: minor
    
    Co-authored-by: Bohdan Chornii <bchornii@softserveinc.com>

--- Tailspin.SpaceGame.Web.Models/Models/FakeViewModel.cs
+++ Tailspin.SpaceGame.Web.Models/Models/FakeViewModel.cs
@@ -12,15 +12,22 @@ namespace TailSpin.SpaceGame.Web.Models
         /// </summary>
         [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
+
         /// <summary>
         /// Name
         /// </summary>
         [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
+
         /// <summary>
         /// Age
         /// </summary>
         [JsonProperty(PropertyName = "age")]
         public int Age { get; set; }
+
+        /// <summary>
+        /// Id
+        /// </summary>        
+        public string ShortAddress { get; set; }
     }
 }
